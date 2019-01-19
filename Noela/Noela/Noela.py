bf71186895c3d687727a56049615ec3a5cd09617
Markus Kuuse <46826542+Nootb@users.noreply.github.com>
2019-01-19 22:46:08 +0200

    V0.2
    starts app on a set of keyword

--- Noela/Noela/Noela.py
+++ Noela/Noela/Noela.py
@@ -3,12 +3,14 @@ import wikipedia
 import wolframalpha
 import pyttsx3
 import speech_recognition as sr
+import os
+import subprocess
 
 engine = pyttsx3.init()
 
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-15)
-engine.say('welcome I am N o e l a')
+engine.say('welcome I  am No el  a')
 engine.say('what can i help you with?')
 engine.runAndWait()
 
@@ -17,7 +19,7 @@ class MyFrame(wx.Frame):
     def __init__(self):        
         #set some basic gui info about the app
         wx.Frame.__init__(self, None,
-            pos=wx.DefaultPosition, size=wx.Size(450, 100),
+            pos=wx.DefaultPosition, size=wx.Size(450, 200),
             style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
             wx.CLOSE_BOX | wx.CLIP_CHILDREN,
             title="Noela")
@@ -54,23 +56,36 @@ class MyFrame(wx.Frame):
         else:            
             try:
                 #wolframalpha:
-                app_id = "Your wolframaplha API"
+                app_id = "Q2HXJ5-GYYYX6PYYP"
                 client = wolframalpha.Client(app_id)
                 res = client.query(input)
                 answer = next(res.results).text 
                 print(answer)
                 say("The answer is '" +answer+ "' ")
-                  except:
+            except:
                 print ("Can't find the answer in wolframalpha, going to next")
                    
             try:
-                input = input.split(' ')            # ~ split input by spacing
-                input = " ".join(input[1:])         # ~ join it, except for the first  word
-                start = open
-                os.system(r'start '+'C:\\Users\\leno\\'+input)     # Opens any file/app  in specified folder (e.g start Desktop\discord) (spacing not supported yet)
+               # Opens any file/app  in specified folder (e.g start Desktop\discord) (spacing not supported yet)
+             if input.startswith('start'):
+                input = input.split(' ')      # ~ split input by spacing
+                input = " ".join(input[1:])   # ~ join it, except for the first  word
                 print("Opening" +input)
+                os.system(r'start '+'C:\\Users\\leno\\' +input)
+            except:
+                print("No files found")
+                 
+            try: 
+             if input == ('open wallpaper engine'):
+                print("Opening" +input)
+                subprocess.call(['C:\\Program Files (x86)\\Steam\\steamapps\\common\\wallpaper_engine\\wallpaper32.exe'])
+            except:
+                print("Can't Open, wrong name")
 
-                
+            try:
+                print("No files found")
+                print("hey")
+
             except:
                 #wikipedia:
                 '''
@@ -86,11 +101,15 @@ class MyFrame(wx.Frame):
                 summary = wikipedia.summary(input)
                 say(" '" +summary+ "' ")
 
+                
+
 def say(text):
     rate = engine.getProperty('rate')
     engine.setProperty('rate', rate+10)
     engine.say(text)
     engine.runAndWait()
+
+
 
 #run the app with the below code
 if __name__ == "__main__":
